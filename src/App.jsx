fb99fd5281e19d532086b27f2b379cad957a8f03
SCPassion <141584825+SCPassion@users.noreply.github.com>
2025-03-20 21:55:39 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+export default function () {
+    return (
+        <main>
+            <header>
+                <h1>Tenzies</h1>
+                <p>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+            </header>
+
+            <button className="btn-roll">Roll</button>
+        </main>
+    )
+}
